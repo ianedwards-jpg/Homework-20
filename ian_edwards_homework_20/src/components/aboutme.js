2d202e9ee430f83f27c7247fb0755a50992ca242
Ian Edwards <ian.edward@uconn.edu>
2020-07-21 00:11:56 -0400

    Update aboutme.js
    Updated About Me Paragraph
    Final Update until build and deploy on Heroku

--- ian_edwards_homework_20/src/components/aboutme.js
+++ ian_edwards_homework_20/src/components/aboutme.js
@@ -20,9 +20,9 @@ const About = () => {
           
           
             <p>
-            I'm a full-stack developer meticulous about creating pixel-perfect applications for clients as well as my own personal projects. I have experience in the front end with everything from ReactJS to Handlebar templating, PWA, HTML5, CSS, JS/Node.JS. My back end experience includes Express/React Router, MySQL, NoSQL, MongoDB, Mongoose and Heroku.
+            I'm a full-stack developer meticulous about creating pixel-perfect applications for clients as well as my own personal projects. I have experience in the front end with everything from ReactJS to Handlebar templating, PWA, HTML5, CSS, JS/Node.JS. My back end experience includes Express/React Router, MySQL, NoSQL, MongoDB, Mongoose and Heroku. Outside of developing, I'm usually playing video games or walking around in the woods. I love to cook and am always ready to go somewhere new!
 
-Education:University of Connecticut, Stamford CT (Full Stack Web Development Bootcamp), Feb-July 2020. University of Connecticut, Storrs, CT (Bachelor Arts, Cognitive Science) Aug 2015-Dec 2017.
+{/* Education:University of Connecticut, Stamford CT (Full Stack Web Development Bootcamp), Feb-July 2020. University of Connecticut, Storrs, CT (Bachelor Arts, Cognitive Science) Aug 2015-Dec 2017. */}
 
 {/* Skill:HTML5, CSS3, SCSS, Javascript, JavaScript Frameworks (React), JQuery, Node, Express, MySQL, NoSQL (MongoDB), AJAX, Git */}
 
